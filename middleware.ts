49d7fa9cadb500316834fcf6f26362bcd45abcff
NKSTUD <n.kourouma@edu.umi.ac.ma>
2024-04-07 19:56:17 +0000

    first commit

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+
+    publicRoutes: [
+        '/',
+        '/events/:id',
+        '/api/webhooks/clerk',
+        '/api/webhooks/stripe',
+        '/api/uploadthing',
+        "/favicon.ico",
+    ],
+
+    ignoredRoutes:[
+        '/api/webhooks/clerk',
+        '/api/webhooks/stripe',
+        '/api/uploadthing'
+    ]
+
+});
+
+export const config = {
+    matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
